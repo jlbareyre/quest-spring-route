f3d53dd89b04b3c226d7afd99e5c30f78f1ce498
Jean-Loup BAREYRE <jlbareyre@gmail.com>
2020-05-29 16:57:59 +0200

    Quest 556 over

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Object doctor(@PathVariable int number) {
 
-        return null;
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker" );
+        } else if ((number>0) && (number<13)) {
+            // Erreur 303 : SEE_OTHER
+            return new ResponseEntity<>(HttpStatus.SEE_OTHER);
+        } else {
+            // Erreur 404 : NOT_FOUND
+            return new ResponseEntity<>("Impossible de récupérer l'incarnation " + number + ".", HttpStatus.NOT_FOUND);
+        }
     }
 }
